fb6f1be0edd6b7f77a8a713f5f40148925a81721
nerd-10 <nilanshgupta103@gmail.com>
2026-08-25 23:41:01 +0530

    started working on player

--- player.py
+++ player.py
@@ -1,1 +1,22 @@
-#$player movement and collision detection
+#player movement and collision detection
+#import pygame as pg
+import settings as st
+import math
+
+class Player:
+    def __init__(self):
+        self.x , self.y = st.PLAYER_POS # x = 5.0 , y = 3.0
+        self.angle = st.PLAYER_ANGLE # angle = 4.71239
+        self.speed = st.PLAYER_SPEED # speed = 2 world units per second
+
+    def get_direction(self):
+        #calculate the direction vector based on the player's angle
+        dir_x = math.cos(self.angle)
+        dir_y = math.sin(self.angle)
+
+        return dir_x , dir_y
+
+
+#p1 = Player()
+#dir_x , dir_y = p1.get_direction()
+#print(f"Player direction: ({dir_x:.2f}, {dir_y:.2f})")
--- settings.py
+++ settings.py
@@ -5,3 +5,9 @@ RES = WIDTH , HEIGHT = 1200 , 600
 HALF_WIDTH , HALF_HEIGHT = WIDTH // 2 , HEIGHT // 2
 FPS = 60
 BACKGROUND_COLOR = (100, 100, 100)  # RGB color for background
+
+#player settings
+
+PLAYER_POS = 5.0, 3.0 #x ,y
+PLAYER_ANGLE = 4.71239 #angle in radians 3 * math.pi / 2, 270°
+PLAYER_SPEED = 2 #movement speed 2 world units per second
