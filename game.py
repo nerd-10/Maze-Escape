6878e9b118eed0ee8b29d3f9d5c14cd744c46682
nerd-10 <nilanshgupta103@gmail.com>
2026-08-29 23:10:28 +0530

    added debug renderer 2D debug view

--- game.py
+++ game.py
@@ -26,6 +26,11 @@ class Game:
 
     def render(self):
         self.screen.fill(st.BACKGROUND_COLOR)  # Clear the screen
+        player_pos = (self.player.x * st.WORLD_SCALE, self.player.y * st.WORLD_SCALE) #player postion to screen postion
+        pg.draw.circle(self.screen, (255, 255, 255), player_pos, st.DEBUG_PLAYER_RADIUS)  # Draw the player as a circle
+        player_direction = self.player.get_direction()
+        line_end = (player_pos[0] + player_direction[0] * st.DEBUG_DIRECTION_LENGTH * st.WORLD_SCALE, player_pos[1] + player_direction[1] * st.DEBUG_DIRECTION_LENGTH * st.WORLD_SCALE) #calculating a direction-line endpoint using a fixed debug length.
+        pg.draw.line(self.screen, (255, 0, 0), player_pos, line_end, 2)  # drawing a line from the player to that endpoint
         # Render game objects here
         pg.display.flip()  # Update the display
 
--- settings.py
+++ settings.py
@@ -5,6 +5,9 @@ RES = WIDTH , HEIGHT = 1200 , 600
 HALF_WIDTH , HALF_HEIGHT = WIDTH // 2 , HEIGHT // 2
 FPS = 60
 BACKGROUND_COLOR = (100, 100, 100)  # RGB color for background
+WORLD_SCALE = 64 #size of a single world unit in pixels
+DEBUG_PLAYER_RADIUS = 10
+DEBUG_DIRECTION_LENGTH = 1.0
 
 #player settings
 
