a656301e277440a55e1b055f2d2e5591b5ad55fe
nerd-10 <nilanshgupta103@gmail.com>
2026-09-10 23:25:47 +0530

    started maze

--- maze.py
+++ maze.py
@@ -1,1 +1,9 @@
 #maze generating algorithm
+#"Given a world position, which maze cell contains it?
+
+def world_to_grid(self, x: float, y: float) -> tuple[int, int]:
+    # convert world coordinates into grid coordinates
+    grid_x = int(x)
+    grid_y = int(y)
+
+    return grid_x, grid_y
